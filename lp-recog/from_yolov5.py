1be7a32ddcd7b04f418c3364363712354a278248
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-04-14 15:11:07 +0000

    Crop_from_txt

---
+++ lp-recog/from_yolov5.py
@@ -0,0 +1,16 @@
+import glob 
+import os
+import cv2 
+
+LABEL_PATH = "/content/yolov5/runs/detect/exp2/labels"
+
+def crop(path):
+    img = os.path.basename(path)
+    print(label)
+    
+    with open(f,'r') as fi:
+        lines = fi.readlines()
+
+if __name__ == "__main__":
+    path = 
+    crop(path)
--- lp-recog/predict_yolov5.sh
+++ lp-recog/predict_yolov5.sh
@@ -4,7 +4,7 @@
 cd /content/yolov5
 
 #predict
-SOURCE="/content/drive/MyDrive/competitions/mosaic-r2/test_car_lp_det"
+SOURCE="/content/drive/MyDrive/competitions/mosaic-r2/test_car_lp_det/OIP(31).jpeg"
 WEIGHTS="/content/drive/MyDrive/competitions/mosaic-r2/weights/yolov5_s/exp2/weights/best.pt"
 CONF="0.4"
 
@@ -12,4 +12,4 @@ echo "Source of images : $SOURCE"
 echo "Weights from : $WEIGHTS"
 echo "Confindance : $CONF"
 
-python3 /content/yolov5/detect.py --source $SOURCE --weights $WEIGHTS --conf $CONF --device "cpu"
+python3 /content/yolov5/detect.py --source $SOURCE --weights $WEIGHTS --conf $CONF --device "cpu" --save-txt
