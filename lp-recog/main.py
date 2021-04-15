f6ebb6e81324a9253e49186904f5319819f1d549
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-04-15 06:26:58 +0000

    Main_img_done

--- lp-recog/main.py
+++ lp-recog/main.py
@@ -1,9 +1,14 @@
 import os
+import cv2
+import shutil
 
 WEIGHTS="/content/drive/MyDrive/competitions/mosaic-r2/weights/yolov5_s/exp2/weights/best.pt"
 CONF="0.4"
+PROJECT=""
+NAME=""
 
 def img(source):
+    global PROJECT, NAME
     root = "/content/predictions/"
     if os.path.isdir(root) == False:
         os.mkdir(root)
@@ -11,41 +16,56 @@ def img(source):
     file_name = os.path.basename(source).split('.')[0]
     PROJECT = os.path.join("/content/predictions",file_name)
 
-    if os.path.isdir(project) == False:
-        os.mkdir(PROJECT)
+    if os.path.isdir(PROJECT) == True:
+        shutil.rmtree(PROJECT, ignore_errors=True)  
+    os.mkdir(PROJECT)
 
     NAME = "yolo_detection"
 
-    print(PROJECT,NAME)
+    # print(PROJECT,NAME)
     
     os.system(f"python3 detect.py --source {source} --weights {WEIGHTS} --conf {CONF} --device 'cpu' --save-txt --project {PROJECT} --name {NAME} --exist-ok")
 
+    crop(source)
+
 def crop(source):
     img_name = os.path.basename(source).split('.')[0]
-    img = cv2.imread(source)
-    height = img.shape[0]
-    width = img.shape[1]
-    print(height,width)
+    image = cv2.imread(source)
+    height = image.shape[0]
+    width = image.shape[1]
+    # print(height,width)
     label_path = os.path.join(PROJECT,NAME,"labels")
     label = os.path.join(label_path,img_name+'.txt')
 
     seg_path = os.path.join(PROJECT,"segmented")
-    
+    if os.path.isdir(seg_path)==True:
+        shutil.rmtree(seg_path, ignore_errors=True)
+    os.mkdir(seg_path)
 
+    # print(label)
     with open(label,'r') as fi:
         lines = fi.readlines()
-    for line in lines:
+
+    # print(lines)
+    
+    for i,line in enumerate(lines):
         param = line.split(' ')
         x = float(param[1])*width
         y = float(param[2])*height
         w = float(param[3])*width
         h = float(param[4])*height
-        print(x,y,w,h)
+        # print(x,y,w,h)
         start_point = (int(x-w/2),int(y-h/2))
         end_point = (int(x+w/2),int(y+h/2))
-        img = cv2.rectangle(img, start_point, end_point, (255, 0, 0), 2)
+        # img = cv2.rectangle(img, start_point, end_point, (255, 0, 0), 2)
+        img = image[start_point[1]:end_point[1],start_point[0]:end_point[0]]
+        # print(img.shape)
 
-        cv2.imwrite(f"/content/{img_name}.jpg",img)
+        #-----segementation----
+        # img = segmentation(img)
+        #------
+        save_path = os.path.join(seg_path,f"lp_{i}.jpg")
+        cv2.imwrite(save_path,img)
 
 if __name__ == "__main__":
     source="/content/drive/MyDrive/competitions/mosaic-r2/test_car_lp_det/several2.jpeg"
